c590445116d54689c19058ece8ca12e8ed1fad8d
Tory Stodard <tory.stodard@gmail.com>
2023-08-10 22:15:48 -0600

    added basic color functionality.

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function createGrid(gridSize) {
   for (let i = 0; i < gridSize * gridSize; i++) {
     const gridCell = document.createElement('div');
     gridCell.className = 'grid-cell';
+    gridCell.addEventListener(
+      'mouseover',
+      () => (gridCell.style.backgroundColor = 'blue')
+    );
     gridContainer.appendChild(gridCell);
   }
 }
