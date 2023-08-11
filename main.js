b26e08f11860f8aeabf0b0a77b8de89b66c5883e
Tory Stodard <tory.stodard@gmail.com>
2023-08-10 22:44:46 -0600

    added change grid function.

--- main.js
+++ main.js
@@ -12,4 +12,17 @@ function createGrid(gridSize) {
   }
 }
 
+function changeGrid(promptStr = 'Enter a grid size between 2 and 100.') {
+  let gridSize = prompt(promptStr);
+  if (gridSize >= 2 && gridSize <= 100) {
+    const gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach((element) => element.remove());
+    createGrid(gridSize);
+  } else if (gridSize === null) {
+    return;
+  } else {
+    changeGrid('You must enter a value between 2 and 100 to change grid size!');
+  }
+}
+
 createGrid(16);
